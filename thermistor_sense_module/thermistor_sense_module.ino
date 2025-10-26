6e3ef2509aaa251562806c4451d0c4595b4a5a82
neeleshroy06 <neelesh.roy06@gmail.com>
2025-10-25 20:04:31 -0700

    ADC to CAN

--- thermistor_sense_module/thermistor_sense_module.ino
+++ thermistor_sense_module/thermistor_sense_module.ino
@@ -1,57 +1,55 @@
 #include "STM32_CAN.h"
 
-const int analogInput = PA0;
-STM32_CAN Can(CAN1, ALT);
-
 static CAN_message_t CAN_outMsg;
+const int analogInput = PA0;  // ADC pin
 
-// ---------- SETUP ----------
-void setup() {
-  pinMode(PB2, OUTPUT);
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("Starting ADC + CAN test...");
+STM32_CAN Can(CAN1, ALT);     // PB8 = RX, PB9 = TX
 
-  Can.begin(true);
-  Can.setBaudRate(500000);
+void SendData(uint16_t voltage_mV) {
+  // Pack the 32-bit voltage value into 4 bytes
+  CAN_outMsg.buf[0] = (voltage_mV & 0xFF);
+  CAN_outMsg.buf[1] = ((voltage_mV >> 8) & 0xFF);
+  CAN_outMsg.buf[2] = 'm';
 
-  // Setup CAN message
-  CAN_outMsg.id = 0x067;
-  CAN_outMsg.len = 8;
-
-  // ADC configuration
-  analogReadResolution(12); // 12-bit ADC
-}
-
-// ---------- CAN SEND FUNCTION ----------
-void SendData(int adcValue) {
-  CAN_outMsg.buf[0] = (unsigned char)(adcValue & 0xFF);
-  CAN_outMsg.buf[1] = (unsigned char)((adcValue >> 8) & 0xFF);
-  CAN_outMsg.buf[2] = (unsigned char)((adcValue >> 16) & 0xFF);
-
-  CAN_outMsg.buf[3] = 'H';
-  CAN_outMsg.buf[4] = 'e';
+  CAN_outMsg.buf[3] = 'V';
+  CAN_outMsg.buf[4] = 'o';
   CAN_outMsg.buf[5] = 'l';
-  CAN_outMsg.buf[6] = 'l';
-  CAN_outMsg.buf[7] = 'o';
+  CAN_outMsg.buf[6] = 't';
+  CAN_outMsg.buf[7] = 's';
 
   Can.write(CAN_outMsg);
 }
 
-// ---------- LOOP ----------
-void loop() {
-  int adcValue = analogRead(analogInput);
-  float voltage = (adcValue / 4095.0) * 3.3;
+void setup() {
+  pinMode(PB2, OUTPUT);   // Just to have a test pin if needed
+  Serial.begin(115200);   // Serial monitor
+  delay(1000);
+  Serial.println("Starting ADC + CAN test...");
 
-  // Print ADC value for validation
+  // Initialize CAN at 500 kbps
+  Can.begin(true);
+  Can.setBaudRate(500000);
+
+  CAN_outMsg.id = 0x076;
+  CAN_outMsg.len = 8;
+
+  analogReadResolution(12); // 12-bit ADC for Blue Pill
+}
+
+void loop() {
+
+  uint16_t adcValue = analogRead(analogInput);
+
+  float voltage = (adcValue * 3.3) / 4095.0;
+  uint16_t voltage_mV = voltage * 1000; 
+
   Serial.print("ADC: ");
   Serial.print(adcValue);
-  Serial.print("   Voltage: ");
-  Serial.print(voltage, 3);
-  Serial.println(" V");
+  Serial.print(" Voltage: ");
+  Serial.print(voltage_mV);
+  Serial.println(" mV");
 
-  // Send over CAN
-  SendData(adcValue);
+  SendData(voltage_mV);
 
-  delay(10000);
+  delay(100);
 }
